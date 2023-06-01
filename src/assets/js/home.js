0a904e494c847e5d60f66cb6a1c6e595b3a00d77
denys_kosyriev <utross2241@gmail.com>
2023-06-01 16:46:52 +0300

    feature: create 'connection-problems' page

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -19,24 +19,4 @@ document.addEventListener('DOMContentLoaded', function () {
     },
   })
 
-  // Show answer 'questions'
-  const questionsList = document.getElementsByClassName('questions-list')[0];
-  const questionsSelects = document.getElementsByClassName('questions-select');
-  const questionsAnswer = document.getElementsByClassName('questions-answer');
-  const questionsIconSelect = document.getElementsByClassName('icon-select');
-
-  Array.from(questionsSelects).map((select, index) => {
-    document.addEventListener('click', (e) => {
-      const closeAnswer = e.composedPath().includes(questionsList);
-      const clickSelect = e.composedPath().includes(select);
-      if (!closeAnswer) {
-        questionsAnswer[index].classList.add('d-none');
-        questionsIconSelect[index].classList.remove('open');
-      } else if (clickSelect) {
-        questionsAnswer[index].classList.toggle('d-none');
-        questionsIconSelect[index].classList.toggle('open');
-      }
-    })
-  })
-
 })
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -73,4 +73,24 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   }
 
+  // Work accordion 'questionsList'
+  const questionsList = document.getElementsByClassName('questions-list')[0];
+  const questionsSelects = document.getElementsByClassName('questions-select');
+  const questionsAnswer = document.getElementsByClassName('questions-answer');
+  const questionsIconSelect = document.getElementsByClassName('icon-select');
+
+  Array.from(questionsSelects).map((select, index) => {
+    document.addEventListener('click', (e) => {
+      const closeAnswer = e.composedPath().includes(questionsList);
+      const clickSelect = e.composedPath().includes(select);
+      if (!closeAnswer) {
+        questionsAnswer[index].classList.add('d-none');
+        questionsIconSelect[index].classList.remove('open');
+      } else if (clickSelect) {
+        questionsAnswer[index].classList.toggle('d-none');
+        questionsIconSelect[index].classList.toggle('open');
+      }
+    })
+  })
+
 });
