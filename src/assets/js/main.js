ab2afad678e526ac332a49e6877aef34827f41cd
denys_kosyriev <utross2241@gmail.com>
2023-06-02 19:04:58 +0300

    feature: create 'all/lozova' and 'tv/lozova' pages

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -94,37 +94,4 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   })
 
-  const modals = document.getElementsByClassName('modal');
-  const tariffs = document.getElementsByClassName('tariff');
-  const modalForm = document.getElementsByClassName('modal-form')[0];
-
-  Array.from(tariffs).map((tariff) => {
-    const connectionButton = tariff.getElementsByClassName('btn')[0];
-    connectionButton.addEventListener('click', () => {
-      if (tariff.className.includes('tariff-internet')) {
-        modalForm.classList.remove('d-none');
-      } else if (tariff.className.includes('tariff-tv')) {
-        modalForm.classList.remove('d-none');
-        modalForm.classList.add('modal-tv');
-      } else if (tariff.className.includes('tariff-all')) {
-        modalForm.classList.remove('d-none');
-        modalForm.classList.add('modal-all');
-      }
-    })
-  })
-
-  Array.from(modals).map((modal, index) => {
-    const closeButton = document.getElementsByClassName('close-modal')[index];
-    const modalContent = document.getElementsByClassName('modal-content')[index];
-    closeButton.addEventListener('click', () => {
-      modal.classList.add('d-none');
-    })
-    modal.addEventListener('click', (e) => {
-      const closeModal = e.composedPath().includes(modalContent);
-      if (!closeModal) {
-        modal.classList.add('d-none');
-      }
-    })
-  })
-
 });
